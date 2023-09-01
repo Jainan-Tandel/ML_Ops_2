c8aaf6550dfe2247c65f262a21ab95716d4fdabd
Jainan-Tandel <jainan11.valsad@gmail.com>
2023-09-01 19:08:41 +0530

    Added split_train_test_dev and predict_and_eval functions

--- exp.py
+++ exp.py
@@ -13,53 +13,25 @@ hand-written digits, from 0-9.
 
 # Import datasets, classifiers and performance metrics
 from sklearn import metrics
-from utils import preprocess_data, split_data, train_model, read_digits
+from utils import preprocess_data, split_data, train_model, read_digits,predict_and_eval,train_test_dev_split
 
 # Read digits: Create a classifier: a support vector classifier
 x,y = read_digits();
 
-# Data splitting: Split data into 70% train and 30% test subsets
-X_train, X_test, y_train, y_test =split_data(x,y, test_size=0.3)
+# Data splitting: Split data into 70% train, 20% test and 10% dev subsets
+X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(x,y, test_size=0.2,dev_size=0.1)
 
 # Data preprocessing
 X_train = preprocess_data(X_train)
 X_test = preprocess_data(X_test)
+X_dev = preprocess_data(X_dev)
 
 #Model training
 model = train_model(X_train, y_train,{'gamma':0.001},model_type="svm")
 
 # Getting model predictions on test set
 # Predict the value of the digit on the test subset
-predicted = model.predict(X_test)
 
-# Evaluation
-# :func:`~sklearn.metrics.classification_report` builds a text report showing
-# the main classification metrics.
+predicted = predict_and_eval(model,X_test,y_test)
+predicted_dev = predict_and_eval(model,X_dev,y_dev)
 
-print(
-    f"Classification report for classifier {model}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
-
-###############################################################################
-
-# true digit values and the predicted digit values.
-
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-
-# The ground truth and predicted lists
-y_true = []
-y_pred = []
-cm = disp.confusion_matrix
-
-# For each cell in the confusion matrix, add the corresponding ground truths
-# and predictions to the lists
-for gt in range(len(cm)):
-    for pred in range(len(cm)):
-        y_true += [gt] * cm[gt][pred]
-        y_pred += [pred] * cm[gt][pred]
-
-print(
-    "Classification report rebuilt from confusion matrix:\n"
-    f"{metrics.classification_report(y_true, y_pred)}\n"
-)
