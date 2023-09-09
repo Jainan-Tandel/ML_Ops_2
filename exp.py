23d1d969604f21f78718793ef8e3e96dcbd4c001
Jainan-Tandel <jainan11.valsad@gmail.com>
2023-09-09 20:43:16 +0530

    Made tune_hparams and removed nested for

--- exp.py
+++ exp.py
@@ -13,13 +13,15 @@ hand-written digits, from 0-9.
 
 # Import datasets, classifiers and performance metrics
 from sklearn import metrics
-from utils import preprocess_data, split_data, train_model, read_digits,predict_and_eval,train_test_dev_split
+from utils import preprocess_data, split_data, train_model, read_digits,predict_and_eval,train_test_dev_split, tune_hparams
+
 
 gamma_ranges = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
 C_ranges = [0.1,1,2,5,10]
+param_list_dict = {'gamma':gamma_ranges,'C':C_ranges}
 
 # Read digits: Create a classifier: a support vector classifier
-x,y = read_digits();
+x,y = read_digits()
 
 # Data splitting: Split data into 70% train, 20% test and 10% dev subsets
 X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(x,y, test_size=0.2,dev_size=0.1)
@@ -31,21 +33,10 @@ X_dev = preprocess_data(X_dev)
 
 #Find best model for given gamma and C range
 
-best_accuracy_so_far = -1
-best_model = None
-for cur_gamma in gamma_ranges:
-    for cur_c in C_ranges:
-        cur_model = train_model(X_train, y_train, {'gamma': cur_gamma,'C':cur_c}, model_type='svm')
-        predicted, cur_accuracy = predict_and_eval(cur_model, X_dev, y_dev,c_report=False,c_matrix=False)
-        if cur_accuracy > best_accuracy_so_far:
-            print("New best accuracy:",cur_accuracy)
-            best_accuracy_so_far = cur_accuracy
-            optimal_gamma = cur_gamma
-            optimal_C = cur_c
-            best_model = cur_model
+best_hparams,best_model, best_accuracy =  tune_hparams(X_train,y_train,X_dev,y_dev,param_list_dict)
 
-print("Optimal C:",optimal_C, "& Optimal gamma:", optimal_gamma)
-
+print("Best hparams:")
+for x in param_list_dict.keys(): print(x,best_hparams.get(x)) 
 
 # Getting model predictions on test set
 # Predict the value of the digit on the test subset
