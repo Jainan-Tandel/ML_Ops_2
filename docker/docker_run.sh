f9630adb21fc9bca69998cb211df3247499b3292
Jainan-Tandel <jainan11.valsad@gmail.com>
2023-11-03 23:50:45 +0530

    Corrected docker_run.sh

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,4 +1,4 @@
 docker build -t digits:v1.1 -f docker/Dockerfile . 
 docker volume create digit_volume
 docker run -d --name Digits_models -v digit_volume:/digits/models digits:v1.1
-docker cp Digits_models:/digits/models/ /home/jainan/VSCode/ML_Ops_2
+docker cp Digits_models:/digits/models /home/jainan/VSCode/ML_Ops_2
