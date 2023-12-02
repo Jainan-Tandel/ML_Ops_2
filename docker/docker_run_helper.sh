c7fb140169d193fd73924443433a4f0552f279ee
Jainan-Tandel <jainan11.valsad@gmail.com>
2023-12-02 19:16:34 +0530

    Helper file final

--- docker/docker_run_helper.sh
+++ docker/docker_run_helper.sh
@@ -1,3 +1,8 @@
 docker build -t base:v2.1 -f docker/DependencyDockerfile . 
 docker build -t digits:v2.1 -f docker/FinalDockerfile .
 docker run -d --name Digits_models -it digits:v2.1
+az acr login --name MLOpsJainan
+docker tag base:v2.1 mlopsjainan.azurecr.io/base:v2.1
+docker tag digits:v2.1 mlopsjainan.azurecr.io/digits:v2.1
+docker push mlopsjainan.azurecr.io/base:v2.1
+docker push mlopsjainan.azurecr.io/digits:v2.1
