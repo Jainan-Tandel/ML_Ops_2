3e9fd2221350120e886f7829c321d0ff852c3b13
Jainan-Tandel <jainan11.valsad@gmail.com>
2023-12-02 15:16:58 +0530

    Correction check 1

--- docker/docker_run_helper.sh
+++ docker/docker_run_helper.sh
@@ -1,3 +1,3 @@
-docker build -t digitsDeps:v1.1 -f docker/DependencyDockerfile . 
-docker build -t digitsFinal:v1.1 -f docker/FinalDockerfile .
-docker run -d --name Digits_models -v $(pwd)/models:/digits/models digitsFinal:v1.1 -it
+docker build -t digitsdeps:v1.1 -f docker/DependencyDockerfile . 
+docker build -t digitsfinal:v1.1 -f docker/FinalDockerfile .
+docker run -d --name Digits_models -v $(pwd)/models:/digits/models digitsfinal:v1.1 -it
