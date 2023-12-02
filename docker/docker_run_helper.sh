ad2461315716d5e432459d0e8be7a49f5b17a505
Jainan-Tandel <jainan11.valsad@gmail.com>
2023-12-02 18:38:42 +0530

    docker Helper Actions test 1

--- docker/docker_run_helper.sh
+++ docker/docker_run_helper.sh
@@ -1,3 +1,3 @@
 docker build -t digitsdeps:v1.1 -f docker/DependencyDockerfile . 
 docker build -t digitsfinal:v1.1 -f docker/FinalDockerfile .
-docker run -d --name Digits_models -v $(pwd)/models:/digits/models digitsfinal:v1.1 -it
+docker run -d --name Digits_models -v $(pwd)/models:/digits/models digitsfinal:v1.1 -t
